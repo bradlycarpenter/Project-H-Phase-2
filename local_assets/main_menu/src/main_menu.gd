bcd565afbec64536db5c5205ba100bf7592f2b87
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-09 18:43:25 +0200

    added/main_menu assets

--- local_assets/main_menu/src/main_menu.gd
+++ local_assets/main_menu/src/main_menu.gd
@@ -8,7 +8,8 @@ extends Control
 @onready var margin_container := $MarginContainer
 @onready var pup_option_menu: SettingMenu = $pup_option_menu
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $MusicStreamPlayer/AudioStreamPlayer2D
-@export_file("*.tscn") var Level
+@export_file("*.tscn") var single_lvl
+@export_file("*.tscn") var multiplayer_lvl
 
 #const LEVEL_1 = preload(Level)
 
@@ -16,9 +17,14 @@ extends Control
 func on_start_pressed() -> void:
 	audio_stream_player_2d.play()
 	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_packed(Level)
+	get_tree().change_scene_to_packed(single_lvl)
+	
+func on_multi_player_button() -> void:
+	audio_stream_player_2d.play()
+	get_tree().change_scene_to_packed(multiplayer_lvl)
 	
 func on_exit_pressed() -> void:
+	audio_stream_player_2d.play()
 	get_tree().quit()
 
 func on_option_pressed() -> void:
@@ -33,6 +39,7 @@ func on_exit_option_menu() -> void:
 
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_pressed)
+	multi_player_button.button_down.connect(on_multi_player_button)
 	option_button.button_down.connect(on_option_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
 	pup_option_menu.exit_options_menu.connect(on_exit_option_menu)

688342438f04252af38212beafc407f19cc769a2
Nazier Abdurahman <nazierabdurahman2@gmail.com>
2024-10-17 21:22:01 +0200

    Updated Item Menu to have dynamic items and added item tiers

--- local_assets/ui/item_menu/src/set_items.gd
+++ local_assets/ui/item_menu/src/set_items.gd
@@ -16,16 +16,18 @@ func _ready() -> void:
 	item1_children = display_item1.get_children()
 	item2_children = display_item2.get_children()
 	item3_children = display_item3.get_children()
-	setItems(item1_children, item1)
-	setItems(item2_children, item2)
-	setItems(item3_children, item3)
-
+	setItems(display_item1, item1_children, item1)
+	setItems(display_item2, item2_children, item2)
+	setItems(display_item3, item3_children, item3)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-func setItems(item_children, item):
+# Legendary Text Effect = "[tornado radius=2 freq=4][color=yellow]" + item.name + "[/color][/tornado]"
+# Epic Text Effect = "[wave amp=15 freq=8][color=purple]" + item.name + "[/color][/wave]"
+func setItems(display_item, item_children, item):
 	item_children[0].texture = item.icon
+	item_children[0].expand = true
 	item_children[1].text = item.name
 	item_children[2].text = item.description
-	
+	display_item.icon = item.tier

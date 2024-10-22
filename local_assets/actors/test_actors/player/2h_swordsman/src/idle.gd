239a85e3813105505e524face008f8622946aa6f
Nazier Abdurahman <nazierabdurahman2@gmail.com>
2024-10-22 17:59:58 +0200

    Pathfinding for bug implemented
    The bug follows the player, and changes sprite based on player direction

--- local_assets/actors/test_actors/player/2h_swordsman/src/idle.gd
+++ local_assets/actors/test_actors/player/2h_swordsman/src/idle.gd
@@ -4,17 +4,17 @@ extends PlayerState
 @export var attack1_state : PlayerState
 
 func enter() -> void:
-    # super()
-    parent.animations.play(animation_name + "_" + parent.last_heading)
+	# super()
+	parent.animations.play(animation_name + "_" + parent.last_heading)
 
 func process_input(_event: InputEvent) -> PlayerState:
-    if Input.is_action_just_pressed("p1_attack_primary"):
-        return attack1_state
-    return null
+	if Input.is_action_just_pressed("p1_attack_primary"):
+		return attack1_state
+	return null
 
 func process_frame(delta: float) -> PlayerState:
-    parent.update_velocity(delta)
-    if parent.velocity != Vector2.ZERO:
-        return run_state
+	parent.update_velocity(delta)
+	if parent.velocity != Vector2.ZERO:
+		return run_state
 
-    return null
+	return null

2b2fe385eb7bdf1c8f11c19de9641b4e8d6774e9
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-22 18:26:17 +0200

    update/load-stats

--- local_assets/ui/hud/scene/player_hud.gd
+++ local_assets/ui/hud/scene/player_hud.gd
@@ -1,13 +1,21 @@
 extends Control
 
-@onready var stats_dmg: RichTextLabel = $MarginContainer/stats_dmg
-
+@onready var stats_dmg: Label = $MarginContainer/stats_dmg
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	stats_dmg.text = "0.5"
-
+	#var stats = FileAccess.open("res://local_assets/actors/items/damage_up.tres", FileAccess.READ)
+	#var dmg_stats = stats.get_as_text()
+	#stats_dmg.text = dmg_stats
+	#var stats = load("res://local_assets/actors/items/damage_up.tres")
+	#var name = stats.stat_boost.damage
+	#stats_dmg.text = str(name)
+	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var stats = load("res://local_assets/actors/items/damage_up.tres")
+	var name = stats.stat_boost.damage
+	stats_dmg.text = str(name)
 	pass

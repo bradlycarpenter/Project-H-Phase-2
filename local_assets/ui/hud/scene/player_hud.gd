efe8fef6c21e069f26eeca1f8d5ad3c04557bd25
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-23 06:56:50 +0200

    update/player-hud-stats-updated

--- local_assets/ui/hud/scene/player_hud.gd
+++ local_assets/ui/hud/scene/player_hud.gd
@@ -1,22 +1,48 @@
 extends Control
 
-@onready var stats_dmg: Label = $MarginContainer/stats_dmg
-@export var stats: Item
+#HP_Bar
+@onready var hp_bar: ProgressBar = $MarginContainer/HBoxContainer/hp_bar
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#var stats = FileAccess.open("res://local_assets/actors/items/damage_up.tres", FileAccess.READ)
-	#var dmg_stats = stats.get_as_text()
-	#stats_dmg.text = dmg_stats
-	#var stats = load("res://local_assets/actors/items/damage_up.tres")
-	#var name = stats.stat_boost.damage
-	#stats_dmg.text = str(name)
-	pass
+##Labels
+@onready var stats_crit: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_crit
+@onready var stats_move_speed: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_move_speed
+@onready var stats_attack_speed: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_attack_speed
+@onready var stats_defense: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_defense
+@onready var stats_dash_count: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_dash_count
+@onready var stats_cd_reduction: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_cd_reduction
+@onready var stats_shield: Label = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/stats_shield
+
+#Data
+@export var stats: Stats
+
+func player_hud_stats() -> void:
 	
+	#hp_bar
+	var hp_value: int = stats.current_health
+	hp_bar.value = hp_value
+	
+	var crit_chance: float = stats.crit_chance
+	stats_crit.text = str(crit_chance)
+	
+	var move_speed: float = stats.move_speed
+	stats_move_speed.text = str(move_speed)
+	
+	var attack_speed: float = stats.attack_speed
+	stats_attack_speed.text = str(attack_speed)
+	
+	var defense: float = stats.defense
+	stats_defense.text = str(defense)
+	
+	var dash: int = stats.dash_count
+	stats_dash_count.text = str(dash)
+	
+	var cooldown_reduction: float = stats.cooldown_reduction
+	stats_cd_reduction.text = str(cooldown_reduction)
+	
+	var shield: float = stats.shield
+	stats_shield.text = str(shield)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#var stats = load("res://local_assets/actors/items/damage_up.tres")
-	var name = stats.stat_boost.damage
-	stats_dmg.text = str(name)
+func _process(_delta: float) -> void:
+	player_hud_stats()
+	#var save = ResourceSaver.save(stats)
 	pass

f9df27c144ca968dff4dd9916df9fbcd8431a4f8
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-23 12:24:51 +0200

    update/placeholders

--- local_assets/ui/hud/scene/player_hud.gd
+++ local_assets/ui/hud/scene/player_hud.gd
@@ -21,28 +21,34 @@ func player_hud_stats() -> void:
 	var hp_value: int = stats.current_health
 	hp_bar.value = hp_value
 	
+	#crit
 	var crit_chance: float = stats.crit_chance
 	stats_crit.text = str(crit_chance)
 	
+	#movement speed
 	var move_speed: float = stats.move_speed
 	stats_move_speed.text = str(move_speed)
 	
+	#attack speed
 	var attack_speed: float = stats.attack_speed
 	stats_attack_speed.text = str(attack_speed)
 	
+	#defense
 	var defense: float = stats.defense
 	stats_defense.text = str(defense)
 	
+	#dsh count
 	var dash: int = stats.dash_count
 	stats_dash_count.text = str(dash)
 	
+	#cooldownredyction
 	var cooldown_reduction: float = stats.cooldown_reduction
 	stats_cd_reduction.text = str(cooldown_reduction)
 	
+	#sheild
 	var shield: float = stats.shield
 	stats_shield.text = str(shield)
 
 func _process(_delta: float) -> void:
 	player_hud_stats()
-	#var save = ResourceSaver.save(stats)
-	pass
+	

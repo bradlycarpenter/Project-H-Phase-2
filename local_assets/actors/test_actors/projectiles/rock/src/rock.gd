b002993dbe74512c730a447069c152352653b54f
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-10-27 14:22:59 +0200

    update/sound_and_dash

--- local_assets/actors/test_actors/projectiles/rock/src/rock.gd
+++ local_assets/actors/test_actors/projectiles/rock/src/rock.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var target_position: Vector2 
 @onready var animations: AnimationPlayer = $AnimationPlayer
+@onready var sfx: AudioStreamPlayer2D = $SFX
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	var tween = create_tween()
@@ -14,7 +16,10 @@ func _ready() -> void:
 		print(target_position)
 	
 	tween.tween_property(self, "global_position", target_position, 1)
+	tween.parallel().tween_property(sprite, "rotation", 360, 1)
 	await tween.finished
+	sfx.play()
+	await sfx.finished
 	queue_free()
 
 func _on_body_entered(body: Player) -> void:

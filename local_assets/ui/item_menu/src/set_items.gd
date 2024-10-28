002e55c985546884c924c5c1aabea65a3c117be9
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-28 19:06:53 +0200

    updated hud

--- local_assets/ui/item_menu/src/set_items.gd
+++ local_assets/ui/item_menu/src/set_items.gd
@@ -110,6 +110,7 @@ func _on_button_pressed() -> void:
 	var selected_item = selected_items[0].stat_boost
 	var player = get_tree().get_nodes_in_group("player")[0]
 	selected_items[0].apply_effect(player)
+	
 
 func _on_button_2_pressed() -> void:
 	var selected_item = selected_items[0].stat_boost
@@ -120,3 +121,8 @@ func _on_button_3_pressed() -> void:
 	var selected_item = selected_items[0].stat_boost
 	var player = get_tree().get_nodes_in_group("player")[0]
 	selected_items[2].apply_effect(player)
+	
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouseButton && event.double_click:
+		event.double_click = false

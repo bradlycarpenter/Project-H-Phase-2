e720f227cdbcf7e43a70d15219c10add99b5cd3a
ChubblesChubbu <42533189+ChubblesChubbu@users.noreply.github.com>
2024-10-31 01:35:25 +0200

    update/scenes

--- local_assets/ui/pause_menu/src/pause_menu.gd
+++ local_assets/ui/pause_menu/src/pause_menu.gd
@@ -1,11 +1,12 @@
-extends Control
+extends CanvasLayer
 
 ##Containers
-@onready var margin_container: MarginContainer = $MarginContainer
 @onready var option_pause_menu: Option_pause_menu = $option_pause_menu
+@onready var margin_container: CanvasLayer = $"."
 
 ##Exports
-@export_file("*.tscn") var main_menu
+@export_file var main_menu: String
+@export var stats: Stats
 
 #region Functions
 
@@ -18,9 +19,9 @@ func pause():
 	get_tree().paused = true
 	
 func testEsc():
-	if Input.is_action_just_pressed("p1_select") and !get_tree().paused:
+	if Input.is_action_just_pressed("p1_start") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("p1_select") and get_tree().paused:
+	elif Input.is_action_just_pressed("p1_start") and get_tree().paused:
 		resume()
 		
 func change_a_scene_manually():
@@ -33,6 +34,20 @@ func _on_resume_pressed() -> void:
 	resume()
 	
 func _on_restart_button_pressed() -> void:
+	
+	#resets stats
+	stats.current_health = 100
+	stats.base_damage = 0
+	stats.crit_chance = 0.1
+	stats.move_speed = 200
+	stats.attack_speed = 1
+	stats.defense = 0
+	stats.dash_count = 1
+	stats.cooldown_reduction = 0
+	stats.double_hit_chance = 0
+	stats.shield = 0
+	stats.dash_attack = false
+	
 	resume()
 	get_tree().reload_current_scene()
 	

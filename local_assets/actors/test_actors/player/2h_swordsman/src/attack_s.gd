c68e7cc9fe82677cb3e7dc3415e0b05da5baed30
Nazier Abdurahman <nazierabdurahman2@gmail.com>
2024-10-30 20:19:06 +0200

    added health potion

--- local_assets/actors/test_actors/player/2h_swordsman/src/attack_s.gd
+++ local_assets/actors/test_actors/player/2h_swordsman/src/attack_s.gd
@@ -5,9 +5,11 @@ var last_direction: Vector2
 
 func enter() -> void:
 	#if parent.stats.dash_attack:
+		parent.base_damage = 30
 		parent.animations.play(animation_name + "_" + parent.last_heading)
 		parent.can_attack = false
 		last_direction = parent.get_input_direction()
+		parent.damage_applied = false
 		pass
 
 func process_input(_event: InputEvent) -> PlayerState:
@@ -21,3 +23,10 @@ func process_physics(delta: float) -> PlayerState:
 	if not parent.animations.is_playing():
 		return idle_state
 	return null
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if not parent.damage_applied and body.is_in_group("mob"):
+		parent.stats.damage = parent.base_damage + parent.stats.base_damage
+		body.apply_damage(parent.stats.damage)
+		parent.damage_applied = true 
